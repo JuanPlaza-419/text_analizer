05d32641427ef0fcccc99a0b0af2e314b5df07c4
Jacobo J. Pedrosa <jacoboj.pedrosa@Jacobos-Laptop.local>
2025-12-11 11:57:16 +0100

    añadidos archivos main y ./text_analizer.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import text_analizer
+
+from text_analizer import word_list
+
+
+
+
+
+if __name__ == "__main__":
+
+    text = input("Introduce a text:")
+
+
---
+++ text_analizer.py
@@ -0,0 +1,13 @@
+
+print("Text Analizer")
+
+
+def word_list(t):
+    pass
+
+
+if __name__ == "__main__":
+    print("Test Text Analizer")
+
+
+    print(word_list("Hola que tal"))
